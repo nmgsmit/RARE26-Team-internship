09ea6cc07d1bd9c851934793b82d7c35caa14211
Smit <n.m.g.smit@student.tue.nl>
2026-03-09 14:09:25 +0100

    Update main.sh for CNN training run

--- main.sh
+++ main.sh
@@ -2,10 +2,8 @@ python3 train.py \
     --data-dir ./data/Challenge_train_data \
     --centers center_1 \
     --batch-size 64 \
-    --epochs 2 \
+    --epochs 20 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "dinov3-center1-training" \
-    --backbone-name vit_base_patch16_dinov3 \
-    --no-pretrained
+    --experiment-id "cnn-center1-training"
