23b14e0e01d114f584448f2d3d7efb056f4c6abe
Smit <n.m.g.smit@student.tue.nl>
2026-03-09 11:17:34 +0100

    Update main.sh: 2 epochs, disable pretrained weights

--- main.sh
+++ main.sh
@@ -4,10 +4,10 @@ python3 train.py \
     --data-dir ./data/Challenge_train_data \
     --centers center_1 \
     --batch-size 64 \
-    --epochs 100 \
+    --epochs 2 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "dinov3-center1-training" \
     --backbone-name vit_base_patch16_dinov3 \
-    --pretrained
+    --no-pretrained
