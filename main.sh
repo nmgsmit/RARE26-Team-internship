d79f8c19eb0bb5ad18a9f389345e30378c8f9252
Smit <n.m.g.smit@student.tue.nl>
2026-03-09 10:53:22 +0100

    Implement DinoV3 model with linear classification head and update training pipeline

--- main.sh
+++ main.sh
@@ -1,10 +1,13 @@
 wandb login
 
 python3 train.py \
-    --data-dir ###FILL IN FOR OUR DATA### \
+    --data-dir ./data/Challenge_train_data \
+    --centers center_1 \
     --batch-size 64 \
     --epochs 100 \
-    --lr 0.001 \
+    --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet-training" \
+    --experiment-id "dinov3-center1-training" \
+    --backbone-name vit_base_patch16_dinov3 \
+    --pretrained
