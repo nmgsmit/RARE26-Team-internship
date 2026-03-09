f8903aee6a42e173fa41d34fa8e18948bd795559
Smit <n.m.g.smit@student.tue.nl>
2026-03-09 14:28:33 +0100

    Apply CNN working changes to main

--- main.sh
+++ main.sh
@@ -1,10 +1,9 @@
-wandb login
-
 python3 train.py \
-    --data-dir ###FILL IN FOR OUR DATA### \
+    --data-dir ./data/Challenge_train_data \
+    --centers center_1 \
     --batch-size 64 \
-    --epochs 100 \
-    --lr 0.001 \
+    --epochs 20 \
+    --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet-training" \
+    --experiment-id "EXPERIMENT_NAME"
