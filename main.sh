e2c8f882c6baa02d31898855f842e623caf6035b
Smit <n.m.g.smit@student.tue.nl>
2026-03-18 14:55:27 +0100

    runnae

--- main.sh
+++ main.sh
@@ -4,10 +4,10 @@ python3 train.py \
     --debug-center1-balanced \
     --debug-class-count 61 \
     --batch-size 64 \
-    --epochs 20 \
+    --epochs 2 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "dinov3-center1-training" \
-    --backbone-name Test_metrics \
+    --experiment-id "Test Metrics" \
+    --backbone-name vit_base_patch16_dinov3 \
     --no-pretrained
